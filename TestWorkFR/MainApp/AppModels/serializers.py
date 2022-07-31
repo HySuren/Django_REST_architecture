05742f531256bd654798641873f09e9f09ff25ca
HySuren <104077284+HySuren@users.noreply.github.com>
2022-08-01 00:26:10 +0300

    Update serializers.py

--- TestWorkFR/MainApp/AppModels/serializers.py
+++ TestWorkFR/MainApp/AppModels/serializers.py
@@ -1,32 +1,33 @@
 from rest_framework import serializers
 from .models import Message, Newsletter, Client
 
-
-class ClientCreate(serializers.ModelSerializer):
-    class Meta:
-        model = Client
-        fields = '__all__'
-
-
-class ClientListSerializator(serializers.ModelSerializer):
-    class Meta:
-        model = Client
-        fields = ('id', 'phone_number', 'tag')
+class Serializers:
+    
+    class ClientCreate(serializers.ModelSerializer):
+        class Meta:
+            model = Client
+            fields = '__all__'
 
 
-class NewsletterCreateSelializator(serializers.ModelSerializer):
-    class Meta:
-        model = Newsletter
-        fields = '__all__'
+    class ClientListSerializator(serializers.ModelSerializer):
+        class Meta:
+            model = Client
+            fields = ('id', 'phone_number', 'tag')
 
 
-class MessageListSelializator(serializers.ModelSerializer):
-    class Meta:
-        model = Message
-        fields = '__all__'
+    class NewsletterCreateSelializator(serializers.ModelSerializer):
+        class Meta:
+            model = Newsletter
+            fields = '__all__'
 
 
-class MessageDetailSelializator(serializers.ModelSerializer):
-    class Meta:
-        model = Message
-        fields = '__all__'
+    class MessageListSelializator(serializers.ModelSerializer):
+        class Meta:
+            model = Message
+            fields = '__all__'
+
+
+    class MessageDetailSelializator(serializers.ModelSerializer):
+        class Meta:
+            model = Message
+            fields = '__all__'
