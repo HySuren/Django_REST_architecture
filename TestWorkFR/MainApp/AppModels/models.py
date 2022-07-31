8b73d169d2e11681ae573aa00b12ff5babf170bd
HySuren <104077284+HySuren@users.noreply.github.com>
2022-08-01 00:16:36 +0300

    Update models.py

--- TestWorkFR/MainApp/AppModels/models.py
+++ TestWorkFR/MainApp/AppModels/models.py
@@ -8,11 +8,6 @@ class Newsletter(models.Model):
     phone_operator = models.CharField('Мобильный оператор', max_length=3, null=False)
     date_end = models.DateTimeField('Дата конца рассылки')
 
-    def __str__(self) -> str:
-        info = self.id, self.messenge, self.date
-        for item in info:
-            info = item
-        return str(info)
 
     class Meta:
         verbose_name = 'Рассылка'
@@ -30,12 +25,6 @@ class Client(models.Model):
         verbose_name = 'Клиент'
         verbose_name_plural = 'Клиенты'
 
-    def __str__(self) -> str:
-        info = self.id, self.phone_number, self.tag
-        for item in info:
-            info = item
-        return str(info)
-
 
 class Message(models.Model):
     id = models.IntegerField(primary_key=True, unique=True)
@@ -43,12 +32,6 @@ class Message(models.Model):
     status = models.CharField('Статус', max_length=25)
     id_Newsletter = models.ForeignKey(Newsletter, on_delete=models.CASCADE)
     id_Сlient = models.ForeignKey(Client, on_delete=models.CASCADE)
-
-    def __str__(self) -> str:
-        info = self.id, self.status, self.date, self.id_Newsletter, self.id_Сlient
-        for item in info:
-            info = item
-        return str(info)
 
     class Meta:
         verbose_name = 'Сообщение'
